563a45ec31b465a3423f8fc84b9cb714ad405af1
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 04:36:17 +0000

    Version 2.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/http-client "2.1.5-SNAPSHOT"
+(defproject org.openvoxproject/http-client "2.1.5"
   :description "HTTP client wrapper"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
