c5e67dac4e68f5848235e9c180f321437bfb177c
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-04-24 09:21:21 +0000

    Version 2.2.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def i18n-version "1.0.4")
 (def slf4j-version "2.0.17")
 
-(defproject org.openvoxproject/http-client "2.2.6"
+(defproject org.openvoxproject/http-client "2.2.7-SNAPSHOT"
   :description "HTTP client wrapper"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
